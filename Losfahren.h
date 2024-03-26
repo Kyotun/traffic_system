8d57f4d2e6b7d7d0a4ca25b282d4372d87ceae5a
Kyotun <emirpisirici@hotmail.com>
2024-03-26 22:58:33 +0100

    Losfahren: Header and source files codes are added.

---
+++ Losfahren.h
@@ -0,0 +1,22 @@
+#ifndef LOSFAHREN_H_
+#define LOSFAHREN_H_
+
+#include "Fahrausnahme.h"
+
+class Weg;
+class Fahrzeug;
+
+class Losfahren : public Fahrausnahme{
+public:
+	// Konstruktoren
+	Losfahren(Fahrzeug& fahrzeug, Weg& weg): Fahrausnahme(fahrzeug, weg){}
+
+	// Voids
+	void vBearbeiten() override;
+
+protected:
+private:
+
+};
+
+#endif /* LOSFAHREN_H_ */
