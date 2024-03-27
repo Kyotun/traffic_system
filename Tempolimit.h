eb4dfb181c18ddb286e649b4bd6f2867389d0903
Kyotun <emirpisirici@hotmail.com>
2024-03-27 10:45:17 +0100

    Tempolimit header added and done.

---
+++ Tempolimit.h
@@ -0,0 +1,12 @@
+#ifndef TEMPOLIMIT_H_
+#define TEMPOLIMIT_H_
+
+#include <limits>
+
+// Vorhandene Tempolimits fuer unterschiedliche Wegtypen.
+enum Tempolimit{
+	Innerorts=50,
+	Landstrasse=100,
+	Autobahn=std::numeric_limits<int>::max()
+};
+#endif /* TEMPOLIMIT_H_ */
