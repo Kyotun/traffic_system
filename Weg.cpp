14551d75bc65e0a1704c658f5382436a824a6713
Kyotun <emirpisirici@hotmail.com>
2024-03-27 12:16:46 +0100

    Weg: Class creation, attributes, constructors, getter&setters.

---
+++ Weg.cpp
@@ -0,0 +1,32 @@
+#include <memory>
+#include <string>
+#include <list>
+#include <iostream>
+#include <iomanip>
+
+#include "Fahrausnahme.h"
+#include "Fahrzeug.h"
+#include "Kreuzung.h"
+#include "Tempolimit.h"
+#include "Weg.h"
+#include "vertagt_liste.h"
+
+using namespace std;
+using namespace vertagt;
+extern double dGlobaleZeit;
+
+// Konstruktor fuer Weg Objekt mit 3 Parametern
+Weg::Weg(string p_sName, double p_dLaenge, Tempolimit p_eTempolimit): // @suppress("Member declaration not found") // @suppress("Class members should be properly initialized")
+		Simulationsobjekt(p_sName), p_dLaenge(p_dLaenge), p_eTempolimit(p_eTempolimit){}
+
+Weg::Weg(string sName, // @suppress("Class members should be properly initialized")
+		double dLaenge,
+		weak_ptr<Kreuzung>pZielkreuzung ,
+		Tempolimit eTempolimit,
+		bool bUeberholverbot) :
+	Simulationsobjekt(sName),
+	p_dLaenge(dLaenge),
+	p_pZielkreuzung(pZielkreuzung),
+	p_eTempolimit(eTempolimit),
+	p_bUeberholverbot(bUeberholverbot) {
+}
---
+++ Weg.h
@@ -0,0 +1,53 @@
+#ifndef WEG_H_
+#define WEG_H_
+
+#include <string>
+#include <memory>
+
+#include "Simulationsobjekt.h"
+#include "Tempolimit.h"
+#include "vertagt_liste.h"
+
+class Fahrzeug;
+class Kreuzung;
+
+using namespace std;
+using namespace vertagt;
+extern double dGlobaleZeit;
+
+class Weg : public Simulationsobjekt{
+public:
+	// Konstruktoren
+	Weg() : Simulationsobjekt(){}
+	Weg(string sName, double dLaenge, Tempolimit eTempolimit = Tempolimit::Autobahn);
+	Weg(string sName, double dLaenge, weak_ptr<Kreuzung>pZielkreuzung ,Tempolimit eTempolimit= Tempolimit::Autobahn, bool bUeberholverbot = true);
+
+	// Getters
+	double getLaenge() const{return p_dLaenge;}
+	double getSchranke()const{return this->p_dSchranke;}
+	VListe<unique_ptr<Fahrzeug>>* getFahrzeugList(){return &p_pFahrzeuge;}
+	bool getUeberholverbot()const{return this->p_bUeberholverbot;}
+
+	// Setters
+	void setLaenge(double dLaenge){this->p_dLaenge = dLaenge;}
+	void setRueckweg(weak_ptr<Weg> pRueckweg){this->p_pRueckweg = pRueckweg;}
+	void setUeberholverbot(bool bUeberholverbot){this->p_bUeberholverbot = bUeberholverbot;}
+	void setSchranke(double dSchranke){this->p_dSchranke = dSchranke;}
+	void setTempolimit(Tempolimit eTempolimit){this->p_eTempolimit = eTempolimit;}
+
+protected:
+private:
+	double p_dLaenge = 0.0;
+	VListe<unique_ptr<Fahrzeug>> p_pFahrzeuge;
+	const weak_ptr<Kreuzung> p_pZielkreuzung;
+	Tempolimit p_eTempolimit = Autobahn;
+	bool p_bUeberholverbot = true;
+	double p_dSchranke = 0.0;
+	weak_ptr<Weg> p_pRueckweg;
+
+};
+
+ostream& operator<<(ostream& ausgabe,const Weg& weg);
+
+
+#endif /* WEG_H_ */
