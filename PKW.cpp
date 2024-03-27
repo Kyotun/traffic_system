1e4fe0845e087ad2c2c1bc98d1223abb2e485b78
Kyotun <emirpisirici@hotmail.com>
2024-03-27 14:50:14 +0100

    PKW: Constructors, getters&setters, attributes.

---
+++ PKW.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <iostream>
+#include <iomanip>
+
+#include "Fahrzeug.h"
+#include "PKW.h"
+#include "SimuClient.h"
+#include "Tempolimit.h"
+#include "Verhalten.h"
+#include "Weg.h"
+
+
+using namespace std;
+extern double dGlobaleZeit;
+
+// Konstruktor mit dem Name, erbt von Fahrzeug
+PKW::PKW(string p_sName): Fahrzeug(p_sName){}
+
+// Konstruktor mit dem Name und  der Geschwindigkeit, erbt von Fahrzeug
+PKW::PKW(string p_sName, double p_dMaxGeschwindigkeit): Fahrzeug(p_sName, p_dMaxGeschwindigkeit){}
+
+// Konstruktor mit dem Verbrauch und Tankvolumen
+PKW::PKW(double p_dVerbrauch, double p_dTankvolumen):
+		p_dVerbrauch(p_dVerbrauch), p_dTankvolumen(p_dTankvolumen){
+	cout << "Ein PKW wurde mit dem Verbrauch '" << p_dVerbrauch
+		 <<"' und der Tankvolumen '" << p_dTankvolumen << "' erzeugt" << endl;
+}
+
+// Konstruktor mit dem Name, der Geschwindigkeit, dem Verbrauch und dem Tankvolumen.
+// Der erbt den Name und die Geschwindigkeit von der Fahrzeugklasse
+PKW::PKW(string p_sName, double p_dMaxGeschwindigkeit,
+				double p_dVerbrauch, double p_dTankvolumen):
+					Fahrzeug(p_sName, p_dMaxGeschwindigkeit),
+					p_dVerbrauch(p_dVerbrauch),
+					p_dTankvolumen(p_dTankvolumen){}
---
+++ PKW.h
@@ -0,0 +1,37 @@
+#ifndef PKW_H_
+#define PKW_H_
+
+#include <string>
+#include <limits>
+#include <iostream>
+
+#include "Fahrzeug.h"
+
+using namespace std;
+
+class PKW : public Fahrzeug {
+public:
+	// Konstruktoren
+	PKW() : Fahrzeug(){}
+	PKW(string p_sName);
+	PKW(string p_sName, double p_dMaxGeschwindigkeit);
+	PKW(double p_dVerbrauch, double p_dTankvolumen = 55.0);
+	PKW(string p_sName, double p_dMaxGeschwindigkeit,
+			double p_dVerbrauch, double p_dTankvolumen = 55.0);
+
+	// Getters
+	double getTankvolumen() const override{return p_dTankvolumen;}
+	double getVerbrauch(){return p_dVerbrauch;}
+
+	// Setters
+	void setVerbrauch(double dVerbrauch){this->p_dVerbrauch = dVerbrauch;}
+	void setTankvolumen(double dTankvolumen){this->p_dTankvolumen = dTankvolumen;}
+
+private:
+	// Double Variablen
+	double p_dVerbrauch = 0.0; // Liter / 100 km
+	double p_dTankvolumen = 55.0; // Default = 55 Liter
+	double p_dTankinhalt = p_dTankvolumen / 2; // Default = 27.5 Liter
+};
+
+#endif /* PKW_H_ */
