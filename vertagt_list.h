386eddd78d8d0c4d19871d4ede558f8ede13aaf5
Kyotun <emirpisirici@hotmail.com>
2024-03-27 11:54:32 +0100

    Vertagt list: getter functions are added.

--- vertagt_list.h
+++ vertagt_list.h
@@ -28,5 +28,50 @@ namespace vertagt
 			p_objekte.clear();
 		}
 
+        void clear()
+		{
+			p_objekte.clear();
+		}
+
+		// Lesefunktionen
+		const_iterator begin() const
+		{
+			return p_objekte.begin();
+		}
+		const_iterator end() const
+		{
+			return p_objekte.end();
+		}
+		iterator begin()
+		{
+			return p_objekte.begin();
+		}
+		iterator end()
+		{
+			return p_objekte.end();
+		}
+
+		const_reverse_iterator rbegin() const
+		{
+			return p_objekte.rbegin();
+		}
+		const_reverse_iterator rend() const
+		{
+			return p_objekte.rend();
+		}
+		reverse_iterator rbegin()
+		{
+			return p_objekte.rbegin();
+		}
+		reverse_iterator rend()
+		{
+			return p_objekte.rend();
+		}
+
+		bool empty() const
+		{
+			return p_objekte.empty();
+		}
+
 	};
 } // namespace vertagt
