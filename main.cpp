d3aed7f215cfef29281df13b77bd558f39662682
Kyotun <emirpisirici@hotmail.com>
2024-03-27 23:24:02 +0100

    Main: Includings are done.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,40 @@
+#include <sstream>
 #include <iostream>
+#include <memory>
+#include <iomanip>
+#include <vector>
+#include <cmath>
+#include <list>
+#include <random>
+#include <limits>
+#include <fstream>
+#include <exception>
+#include <stdexcept>
+
+#include "Fahrausnahme.h"
+#include "Fahren.h"
+#include "Fahrrad.h"
+#include "Fahrzeug.h"
+#include "Kreuzung.h"
+#include "Losfahren.h"
+#include "Parken.h"
+#include "PKW.h"
+#include "SimuClient.h"
+#include "Simulation.h"
+#include "Simulationsobjekt.h"
+#include "Streckenende.h"
+#include "Tempolimit.h"
+#include "Verhalten.h"
+#include "vertagt_aktion.h"
+#include "vertagt_liste.h"
+#include "Weg.h"
 
 using namespace std;
-double dGlobalTime = 0.0;
+double dGlobaleZeit = 0.0;
 
 void vAufgabe_1();
 
 int main(){
-    return 0;
-}
-
-void vAufgabe_1(){
+	// vAufgabe_1();
+	return 0;
 }
